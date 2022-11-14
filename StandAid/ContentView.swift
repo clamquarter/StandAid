9a4ef0a77cd315f7ef78b68cec6a440fbd62d2e4
India Doria <indiadoria@gmail.com>
2022-11-14 15:37:15 -0500

    Initial Commit

---
+++ StandAid/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StandAid
+//
+//  Created by India Doria on 11/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StandAid/StandAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandAidApp.swift
+//  StandAid
+//
+//  Created by India Doria on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct StandAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
