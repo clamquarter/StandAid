0e47826921e7b2a437678fe66936f065998f080d
India Doria <indiadoria@gmail.com>
2022-11-14 15:55:22 -0500

    i changed something.

--- StandAid/ContentView.swift
+++ StandAid/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("I changed stuff.")
         }
         .padding()
     }
